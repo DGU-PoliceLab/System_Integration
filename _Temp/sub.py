bbc5181d3c4d303ad21e7e516d54447293473138
mamensu_ <qqaazz0222@naver.com>
2024-06-28 06:35:50 +0000

    Chore: Remove pipe test files

--- _Temp/sub.py
+++
@@ -1,8 +0,0 @@
-def sub_process1(pipe):
-    p = pipe.recv()
-    print(f"sub process1 recv data: {p}")
-
-def sub_process2(pipe):
-    while True:
-        p = pipe.recv()
-        print(f"sub process2 recv data: {p}")
